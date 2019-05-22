474c7c701bc5088b8c06726fbdc0a0240b4cd615
yotammanne <yotammanne@mail.tau.ac.il>
2019-05-22 12:01:44 +0300

    grammar3 is non cnf for q2b, finished q2a, modified sentence for q2b

--- cky.py
+++ cky.py
@@ -30,13 +30,15 @@ def cnf_cky(pcfg, sent):
     n = len(sent)
     non_terminals = [nt for nt in pcfg._rules if not pcfg.is_terminal(nt)]
     binary_rules = get_binary_rules(pcfg)
-    for i in range(n):
+    for i in range(1,n+1):
         for X in non_terminals:
             for rule in pcfg._rules[X]:
-                pi[(i,i,X)] = rule[1]/pcfg._sums[X] if sent[i] in rule[0][0] else 0
+                pi[(i,i,X)] = rule[1]/pcfg._sums[X] if sent[i-1] in rule[0][0] else 0.0
+                if pi[(i, i, X)] > 0.0:
+                    break
 
-    for l in range(n-1):
-        for i in range(n-l):
+    for l in range(1,n):
+        for i in range(1,n-l+1):
             j = i+l
             for X in non_terminals:
                 max_prob = float('-inf')
@@ -52,16 +54,82 @@ def cnf_cky(pcfg, sent):
                         if prob > max_prob:
                             max_prob = prob
                             pi[(i,j,X)] = max_prob
-                            bp[(i,j,X)] = binary_rule
+                            bp[(i,j,X)] = (binary_rule,s)
 
+    try:
+        tree = create_tree(bp,pi,n,sent)
+        return tree
+    except:
+        pass
     ### END YOUR CODE
     return "FAILED TO PARSE!"
 
+### not modified yet - identical to cnf_cky ###
 def non_cnf_cky(pcfg, sent):
     ### YOUR CODE HERE
-    raise NotImplementedError
+    pi = dict()
+    bp = dict()
+    sent = sent.split(' ')
+    n = len(sent)
+    non_terminals = [nt for nt in pcfg._rules if not pcfg.is_terminal(nt)]
+    binary_rules = get_binary_rules(pcfg)
+    for i in range(1,n+1):
+        for X in non_terminals:
+            for rule in pcfg._rules[X]:
+                pi[(i,i,X)] = rule[1]/pcfg._sums[X] if sent[i-1] in rule[0][0] else 0.0
+                if pi[(i, i, X)] > 0.0:
+                    break
+
+    for l in range(1,n):
+        for i in range(1,n-l+1):
+            j = i+l
+            for X in non_terminals:
+                max_prob = float('-inf')
+                if X not in binary_rules:
+                    continue
+                for binary_rule in binary_rules[X]:
+                    for s in range(i,j):
+                        q = binary_rule[1]/pcfg._sums[X]
+                        if (i,s,binary_rule[0][0]) in pi and (s+1,j,binary_rule[0][1]) in pi:
+                            prob = q * pi[(i,s,binary_rule[0][0])] * pi[(s+1,j,binary_rule[0][1])]
+                        else:
+                            prob = 0.0
+                        if prob > max_prob:
+                            max_prob = prob
+                            pi[(i,j,X)] = max_prob
+                            bp[(i,j,X)] = (binary_rule,s)
+
+    try:
+        tree = create_tree(bp,pi,n,sent)
+        return tree
+    except:
+        pass
     ### END YOUR CODE
     return "FAILED TO PARSE!"
+
+
+def create_tree(bp,pi,n,sent):
+    return create_tree_rec(1, n, 'ROOT', bp, pi, sent)
+
+
+def create_tree_rec(i,j,symbol,bp,pi,sent):
+    if pi[(i,j,symbol)] == 0.0:
+        raise KeyError
+    if i == j:
+        # if pi[(i,j,symbol)] == 0.0:
+        #     raise KeyError
+        leaf = '(' + symbol + ' ' + sent[i-1] + ')'
+        return leaf
+    s = bp[(i, j, symbol)][1]
+    expansion = bp[(i, j, symbol)][0][0]
+    if i == s:
+        left = symbol + ' ' + '(' + expansion[0] + ' ' + sent[i-1] + ')'
+        right = create_tree_rec(s+1,j,expansion[1],bp,pi,sent)
+        return '(' + left + ' ' + right + ')'
+    left = create_tree_rec(i,s,expansion[0],bp,pi,sent)
+    right = create_tree_rec(s+1,j,expansion[1],bp,pi,sent)
+    return '('+ symbol + ' ' + left + ' ' + right + ')'
+
 
 if __name__ == '__main__':
     import sys
@@ -69,5 +137,5 @@ if __name__ == '__main__':
     non_cnf_pcfg = PCFG.from_file_assert(sys.argv[2])
     sents_to_parse = load_sents_to_parse(sys.argv[3])
     for sent in sents_to_parse:
-        print cnf_cky(cnf_pcfg, sent)
+#        print cnf_cky(cnf_pcfg, sent)
         print non_cnf_cky(non_cnf_pcfg, sent)
